4f1c51ebb0d6223c8d1bd210833efd55c888595f
Jean-Michel Garnier <jean-michel@21croissants.com>
2012-06-06 17:23:33 +0200

    Copy link to clipboard feature

--- README.md
+++ README.md
@@ -1,65 +1,47 @@
-google
-======
+google-copy-link
+================
 
-## The power of Google Search in your command line.
+The CLI displays results from the [Google Web Search API](https://developers.google.com/web-search/), allows you to page through the results, and choose a url to copy to your clipboard, all without leaving the command line.
 
-The CLI displays results from the [Google Web Search API](https://developers.google.com/web-search/), allows you to page through the results, and choose a page to view, all without leaving the command line.
-
-[Hosted on RubyGems](https://rubygems.org/gems/google).
-
-[![Screenshot of `google` in action!](https://github.com/Kerrick/google/raw/master/screenshots/example_01.gif)](https://github.com/Kerrick/google/raw/master/screenshots/example_01.gif "Click to view full size")
+[![Screenshot of `goo` in action!](https://github.com/21croissants/google/raw/master/screenshots/example_01.jpg)](https://github.com/21croissants/google/raw/master/screenshots/example_01.jpg "Click to view full size")
 
 ## Installation
 
-    gem install google # Requires Ruby 1.9
+    gem install google-copy-link # Requires Ruby 1.9
 
 ## Usage Examples
 
-Typing `google --help` will list all the available commands. They are also listed here.
+Typing `goo --help` will list all the available commands. They are also listed here.
 
-    The google gem is a simple tool to search Gooogle with via a CLI.
+    The goo gem is a simple tool to search Gooogle and copy a search result link to your clipboard.
     Usage:
-        google [options] "my search query string here"
+        goo [options] "my search query string here"
     where [options] are:
+          --markdown, -m:     Use markdown format when copying link to clipboard. Example: \[Trollop\]\(http://trollop.rubyforge.org/\) (Default: false)
             --page, -p <i>:   Start by showing the <i>th result page. (Default: 1)
             --size, -s <i>:   Show <i> results on each SERP. Must be between 1 and 8. (Default: 4)
           --result, -r <i>:   Skip the SERP and show the <i>th result.
-               --lucky, -l:   I'm feeling lucky! Skip the SERP and show the first result. (Alias to --result 1)
-      --no-readability, -e:   Filter the results through readability to get rid of extra content. (Default: true)
-         --no-markdown, -m:   Change the results from raw HTML to markdown. (Default: true)
-             --version, -v:   Print the version and exit.
                 --help, -h:   Show this information and exit.
 
-### I'm Feeling Lucky into Less
+At the prompt, type the index of a search result, it will be copied to your clipboard.
 
-    google -l 'how to install nvidia drivers debian squeeze' | less
+If you press <Return>, the first search result link will be copied.
+
+*ONLY TESTED with a mac*. Should work on linux and windows thanks to clipboard gem;)
+
+When writing blog posts in Markdown or looking for libraries in google, I became tired to repeat the whole manual search process in the browser so I found a way to automate it;)
 
 ### Search on a single site
 
-    google 'site:randsinrepose.com NADD'
-
-### Vanity Search
-
-    google '"Kerrick Long" -inurl:kerrick -inurl:kerricklong'
+    goo 'site:randsinrepose.com NADD'
 
 ## Features
 
 * You have access to all the [search operators](http://support.google.com/websearch/bin/answer.py?hl=en&answer=136861) you've come to know and love in Google Search.
 
-* If using the `--lucky` or `--result` flags, you can pipe the results into any other unix command.
-
-* Results are filtered through [Readability](http://www.readability.com/), so you only get the relevant content. (Shoutout to [Ruby Readability](https://github.com/iterationlabs/ruby-readability)!)
-
 * Results are shown in [markdown](http://daringfireball.net/projects/markdown/) for a good balance between legibility and document heirarchy. (Shoutout to [reverse_markdown](https://github.com/xijo/reverse_markdown)!)
 
 * Results pages are formatted to look like a Google SERP, including colors, domains, descriptions, and bold search matches. (Shoutout to [formatador](https://github.com/geemus/formatador)!)
-
-* You can pipe a result into another unix command! At the prompt, type the number of the result, followed by a pipe as normal. Note that utilities such as `less` and `more` that need to control the display don't work.
-
-For example,
-
-    Enter N or P for pagination, E or Q to quit, or a number to see that result.
-    > 3 | espeak -a 200 -v en-us
 
 ## Supported Ruby Versions
 
@@ -67,21 +49,17 @@ This library has only been tested against Ruby 1.9.3. Other versions might be su
 
 ## Acknowledgements
 
+Most of the code is a fork of Kerrick Long. See [LICENSE](https://github.com/21croissants/google/blob/master/LICENSE.md) for details.
+
 This gem is [Powered by Google](http://www.google.com).
 
 ### Copyright
 
-Copyright (c) 2012 Kerrick Long. See [LICENSE](https://github.com/Kerrick/google/blob/master/LICENSE.md) for details.
+Copyright (c) 2012 Jean-Michel Garnier. See [LICENSE](https://github.com/21croissants/google/blob/master/LICENSE.md) for details.
 
 ### Dependencies
 
 * [Trollop](http://trollop.rubyforge.org/) (gem)
-
-* [Ruby Readability](https://github.com/iterationlabs/ruby-readability) (gem)
-
-    * [Nokogiri](http://nokogiri.org/)
-
-    * [guess_html_encoding](https://github.com/iterationlabs/guess_html_encoding)
 
 * [JSON Implementation for Ruby](http://flori.github.com/json/) (gem)
 
@@ -90,3 +68,5 @@ Copyright (c) 2012 Kerrick Long. See [LICENSE](https://github.com/Kerrick/google
 * [HTML Entities for Ruby](http://htmlentities.rubyforge.org/) (gem)
 
 * [Reverse Markdown](https://github.com/xijo/reverse_markdown) (lib)
+
+* [clipboard gem](https://github.com/janlelis/clipboard)
--- lib/google.rb
+++ lib/google.rb
@@ -6,12 +6,13 @@ require 'open-uri'
 require 'json'
 require 'formatador'
 require 'htmlentities'
-require 'ruby-readability'
+require 'clipboard'
 
 class Google
   def initialize(query, opts)
-    @query       = URI.escape(query)
-    @opts        = opts
+    @unescaped_query = query
+    @query = URI.escape(query)
+    @opts = opts
     @opts[:size] = 8 if opts[:size] > 7
     @opts[:size] = 1 if opts[:size] <= 1
   end
@@ -29,7 +30,7 @@ class Google
   def request(query_strings)
     @api_url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0"\
                "&rsz=#{query_strings[:rsz]}&start=#{query_strings[:start]}&q=#{query_strings[:q]}"
-    results  = JSON.parse(open(@api_url).read)
+    results = JSON.parse(open(@api_url).read)
     if results['responseStatus'].to_i != 200
       Trollop::die Utils::wrap("Google Search API Status #{results['responseStatus']}. Details:\n"\
                                "#{results['responseDetails']}\nTry again in a moment")
@@ -39,23 +40,23 @@ class Google
   end
 
   def display_serp(info)
-    results           = info[:results]
-    query_strings     = info[:query_strings]
-    coder             = HTMLEntities.new
-    current_page      = results['responseData']['cursor']['currentPageIndex']+1
-    max_result        = query_strings[:start] + query_strings[:rsz]
+    results = info[:results]
+    query_strings = info[:query_strings]
+    coder = HTMLEntities.new
+    current_page = results['responseData']['cursor']['currentPageIndex']+1
+    max_result = query_strings[:start] + query_strings[:rsz]
     estimated_results = results['responseData']['cursor']['resultCount']
-    result_array      = results['responseData']['results']
+    result_array = results['responseData']['results']
 
     Formatador.display_line "\n#{' ' * (max_result.to_s.length + 2)}[yellow]Powered by Google[/]"
 
     result_array.each_with_index do |result, i|
       this_num = (i + query_strings[:start] + 1).to_s
 
-      serp_title  = "\n#{' ' * (max_result.to_s.length - this_num.length)}[bold][blue]#{this_num}. "\
+      serp_title = "\n#{' ' * (max_result.to_s.length - this_num.length)}[bold][blue]#{this_num}. "\
                     "[normal]#{result["titleNoFormatting"]}[/]\n"
-      serp_url    = "#{' ' * max_result.to_s.length}[green]#{result["url"]}[/]\n"
-      serp_desc   = ' ' * max_result.to_s.length + result["content"].gsub(/<b>/, "[bold]").gsub(/<\/b>/, "[/]").squeeze(" ")
+      serp_url = "#{' ' * max_result.to_s.length}[green]#{result["url"]}[/]\n"
+      serp_desc = ' ' * max_result.to_s.length + result["content"].gsub(/<b>/, "[bold]").gsub(/<\/b>/, "[/]").squeeze(" ")
 
       Formatador.display_line coder.decode(Utils::wrap(serp_title, :prefix => max_result.to_s.length + 2))
       Formatador.display_line coder.decode(Utils::wrap(serp_url, :prefix => max_result.to_s.length + 2))
@@ -69,7 +70,7 @@ class Google
   end
 
   def input(info, result_array)
-    Formatador.display Utils::wrap("\n[yellow]Enter N or P for pagination, E or Q to quit, or a number to see that result.")
+    Formatador.display Utils::wrap("\n[yellow]Enter N or P for pagination, E or Q to quit, or a number to copy the url to your clipboard.")
     Formatador.display "\n[bold]>[/] "
     choice = STDIN.gets
     if choice.nil? # Likely because the user submitted EOT via ^D
@@ -79,61 +80,49 @@ class Google
     end
 
     case
-    # Quit
-    when choice[0].downcase == 'e' || choice[0].downcase == 'q'
-      exit
-    # Next Page
-    when choice[0].downcase == 'n'
-      display_serp  request(:q => @query, :rsz => @opts[:size], :start => info[:query_strings][:start] + @opts[:size])
-    # Previous Page
-    when choice[0].downcase == 'p'
-      if info[:query_strings][:start] < 1
-        Formatador.display Utils::wrap("[yellow]! Already at page one.")
-        input info, result_array
-      else
-        display_serp request(:q => @query, :rsz => @opts[:size], :start => info[:query_strings][:start] - @opts[:size])
-      end
-    # Numerical Choice
-    when choice[0].match(/\d/)
-      /(\d+)(\s*\|(.*))*/.match(choice) do |str|
-        num = str[1].to_i - 1 # Remember, we are 1-indexing things for the user
-        # Is the result on the current page?
-        if info[:query_strings][:start] <= num && info[:query_strings][:start] + @opts[:size] > num
-          # Is the user piping the result to something?
-          if str[3].nil?
-            view result_array[num - info[:query_strings][:start]]['url']
-          else
-            pipe str[3].strip, result_array[num - info[:query_strings][:start]]['url']
-          end
-        else
-          Formatador.display Utils::wrap("[yellow]! Result not on this page.")
+      # Quit
+      when choice[0].downcase == 'e' || choice[0].downcase == 'q'
+        exit
+      # Next Page
+      when choice[0].downcase == 'n'
+        display_serp request(:q => @query, :rsz => @opts[:size], :start => info[:query_strings][:start] + @opts[:size])
+      # Previous Page
+      when choice[0].downcase == 'p'
+        if info[:query_strings][:start] < 1
+          Formatador.display Utils::wrap("[yellow]! Already at page one.")
           input info, result_array
+        else
+          display_serp request(:q => @query, :rsz => @opts[:size], :start => info[:query_strings][:start] - @opts[:size])
         end
-      end
-    # Catch-all to grab input again
+      # Numerical Choice
+      when choice[0].match(/\d/)
+        /(\d+)(\s*\|(.*))*/.match(choice) do |str|
+          #puts str.inspect
+
+          num = str[1].to_i - 1 # Remember, we are 1-indexing things for the user
+
+          clipboard_copy url(result_array, info, num)
+        end
+      # Catch-all to grab input again
+      else
+        clipboard_copy url(result_array, info, 0)
+    end
+    exit
+  end
+
+  def clipboard_copy(plain_url, markdown_link_format = @opts[:markdown])
+    text = if markdown_link_format
+      "[#{@unescaped_query}](#{plain_url})"
     else
-      input info, result_array
+      plain_url
     end
+    Clipboard.copy text
   end
 
-  def grab(url)
-    source = open(url).read
-    content = @opts[:readability] ? Readability::Document.new(source,
-                                                              :tags => %w[div p a pre code h1 h2 h3 h4 h5 h6 blockquote ul ol li],
-                                                              :attributes => %w[href],
-                                                              :remote_empty_nodes => true).content : source
-    output = @opts[:markdown] ? ReverseMarkdown.new.parse_string(content) : content
-    output
+  def url(result_array, info, obscure_num)
+    result_array[obscure_num - info[:query_strings][:start]]['url']
   end
 
-  def pipe(command, url)
-    text = grab(url)
-    IO.popen(command, 'w+') do |process|
-      process.write(text)
-      process.close_write
-      puts process.read
-    end
-  end
 
   def view(url)
     Formatador.display Utils::wrap(grab(url))
